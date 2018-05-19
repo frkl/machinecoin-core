d3e415df6819c17fcfb164c37aeec09de05c0e7c
frkl <name@example.org>
2018-05-19 11:12:02 -0400

    getwork

---
+++ run.sh
@@ -0,0 +1,1 @@
+/work/whattomine/wallet/mac/src/machinecoind --datadir=/work/whattomine/wallet-data/mac-timetravel -server=1 -rpcuser=grrr -rpcpassword=13457 -rpcport=6111 -port=8111  -txindex=1 -reindex  &
